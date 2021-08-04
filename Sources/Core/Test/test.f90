b892caab2d0509e0c1e826126a9d93bc3432c11d
garamKim <8718033x@gmail.com>
2021-08-04 11:56:18 +0900

    create initial build structure

---
+++ Sources/Core/Test/test.f90
@@ -0,0 +1,13 @@
+module mathOps
+  use cudafor
+    implicit none
+contains
+    attributes(global) subroutine saxpy(x, y, a)
+        real :: x(:), y(:)
+        real, value :: a
+        integer :: i, n
+        n = size(x)
+        i = blockDim%x*(blockIdx%x - 1) + threadIdx%x
+        if (i <= n) y(i) = y(i) + a*x(i)
+    end subroutine saxpy
+end module mathOps
