8b95c5f1ac87282fd93926f29f10dc29f8e92ae6
garam <bumdori2@gnu.ac.kr>
2021-08-11 11:21:25 +0900

    yeah

--- Sources/Core/Solver/DiffusionSolver/DiffusionSolver.f90
+++ Sources/Core/Solver/DiffusionSolver/DiffusionSolver.f90
@@ -1,13 +1,20 @@
 module DiffusionBase
-    use, intrinsic :: iso_fortran_env
-    implicit none
+  use, intrinsic :: iso_fortran_env
+  implicit none
 
-    type, abstract :: DiffusionSolver
-    contains
-    end type DiffusionSolver
+  type, abstract :: DiffusionSolver
+   contains
+  end type DiffusionSolver
 
-    interface 
-    end interface
+  type, abstract :: TransportSolver
+  end type TransportSolver
+
+contains
+
+
+
+
+
 
 
 end module DiffusionBase
