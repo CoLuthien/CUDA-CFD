6aa273c7bec2c285cced0a5f15961a9b3393cdfa
garam <bumdori2@gnu.ac.kr>
2021-08-16 16:27:44 +0900

    test

--- Examples/HelloCuda/main.f90
+++ Examples/HelloCuda/main.f90
@@ -1,8 +1,27 @@
 program testSaxpy
     use :: Species
+    use :: ArrayBase
     implicit none
     integer, parameter :: N = 40000
     real :: x(N), y(N), a
     real :: x_d(N), y_d(N)
+
+    call test()
+
+contains 
+    subroutine test()
+    implicit none
+    type(Array4), allocatable :: arr1, arr3 
+    type(Array4) :: arr2, arr4, arr5
+    arr1 = Array(100, 100, 100, 200)
+    arr2 = Array(100, 100, 100, 200)
+
+
+    call move(arr1, arr4)
+    call move(arr2, arr5)
+    pause
+    
+    end subroutine test
+    
 
 end program testSaxpy
